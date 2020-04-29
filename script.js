76fb6a4199d63e8848061e3324f6724dded985af
harfang <raupasto@gmail.com>
2020-04-29 19:57:04 +0200

    click ok mais le troisième click bug

--- script.js
+++ script.js
@@ -15,10 +15,47 @@ var img =document.getElementsByTagName('img');
 
 for(let i=0; i <img.length; i++){
     img[i].src2 = 'img/pic' + deck[i] + '.jpg';
-    img[i].addEventListener('click', function (e){
-        e.target.src = e.target.src2;
-    })
-}
+    
+    }
+
+var step =1;
+var p1,p2;
+
+document.addEventListener('click', function(e){
+
+    switch(step){
+        case 1: /* premier clique*/ 
+            if (e.target.tagName=='IMG'){
+                e.target.src = e.target.src2;
+                p1 = e.target;
+                step = 2;
+            }
+
+            break;
+
+        case 2: /*deuxième clique*/ 
+            if (e.target.tagName=='IMG'){
+                e.target.src = e.target.src2;
+                p2 = e.target;
+                step = 3;
+            }
+
+            break;
+
+        case 3:
+            if (p1.src2==p2.src2){
+                p1.replaceWith( document.createElement('span'))
+                p2.replaceWith( document.createElement('span'))
+            }
+
+            else{
+                p2.src = p1.src ='img/pic0.jpg';
+            }
+
+            step =1;
+            break;
+    }
+})
 
 
      
